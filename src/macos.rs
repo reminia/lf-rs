fa12ebcd079f8f5e7679e78eefba53e4f3a79770
sleefd <sleefd@gmail.com>
2024-03-29 11:27:43 +0800

    refactor: extract applescript out

--- src/macos.rs
+++ src/macos.rs
@@ -1,15 +1,20 @@
 use std::process::Command;
 
-// leverage AppleScript to focus on the file
-pub fn open_folder_and_select_items(item_paths: &[&str]) -> bool {
-    let mut script = String::new();
-    script.push_str("tell application \"Finder\"\n");
-    script.push_str(&format!("activate\n"));
-    for item_path in item_paths {
-        script.push_str(&format!("select POSIX file \"{}\" as alias\n", item_path));
-    }
-    script.push_str("end tell\n");
+fn applescript(files: &[&str]) -> String {
+    format!(
+        "tell application \"Finder\"\n\
+         activate\n\
+         {}\n\
+         end tell",
+        files
+            .iter()
+            .map(|file| format!("select POSIX file \"{}\" as alias", file))
+            .collect::<String>()
+    )
+}
 
+pub fn open_folder_and_select_items(paths: &[&str]) -> bool {
+    let script = applescript(paths);
     let script_args = vec!["-e", &script];
     let result = Command::new("osascript")
         .args(&script_args)
