6b7d9dfe75fd346695eeefb7dae5159b60631297
sleefd <sleefd@gmail.com>
2024-03-29 22:08:41 +0800

    feat: add message to warn user

--- src/windows.rs
+++ src/windows.rs
@@ -1,6 +1,9 @@
 use std::process::Command;
 
 pub fn open_folder_and_select_items(paths: &[&str]) -> bool {
+    if paths.len() > 1 {
+        println!("windows supports to select only 1 file.")
+    }
     let mut cmd = Command::new("explorer.exe");
     let arg = format!("/select,{}", paths.join(","));
     cmd.arg(arg);
