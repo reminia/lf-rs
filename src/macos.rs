f1997001d189fc94ac297610107c34223edb034b
sleefd <sleefd@gmail.com>
2024-03-29 10:46:14 +0800

    feat: open curr dir if no files are specified and cargo fmt

--- src/macos.rs
+++ src/macos.rs
@@ -2,19 +2,19 @@ use std::process::Command;
 
 // leverage AppleScript to focus on the file
 pub fn open_folder_and_select_items(item_paths: &[&str]) -> bool {
-  let mut script = String::new();
-  script.push_str("tell application \"Finder\"\n");
-  script.push_str(&format!("activate\n"));
-  for item_path in item_paths {
-      script.push_str(&format!("select POSIX file \"{}\" as alias\n", item_path));
-  }
-  script.push_str("end tell\n");
+    let mut script = String::new();
+    script.push_str("tell application \"Finder\"\n");
+    script.push_str(&format!("activate\n"));
+    for item_path in item_paths {
+        script.push_str(&format!("select POSIX file \"{}\" as alias\n", item_path));
+    }
+    script.push_str("end tell\n");
 
-  let script_args = vec!["-e", &script];
-  let result = Command::new("osascript")
-      .args(&script_args)
-      .output()
-      .expect("Failed to execute AppleScript");
+    let script_args = vec!["-e", &script];
+    let result = Command::new("osascript")
+        .args(&script_args)
+        .output()
+        .expect("Failed to execute AppleScript");
 
-  result.status.success()
+    result.status.success()
 }
