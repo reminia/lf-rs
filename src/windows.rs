155e3c31a14ca4cdb2b0804754fe684436ca3a3b
sleefd <sleefd@gmail.com>
2024-03-29 22:10:00 +0800

    chore: fmt code

--- src/windows.rs
+++ src/windows.rs
@@ -7,9 +7,7 @@ pub fn open_folder_and_select_items(paths: &[&str]) -> bool {
     let mut cmd = Command::new("explorer.exe");
     let arg = format!("/select,{}", paths.join(","));
     cmd.arg(arg);
-    let result = cmd
-        .output()
-        .expect("Failed to run explorer.exe command");
+    let result = cmd.output().expect("Failed to run explorer.exe command");
 
     result.status.success()
 }
