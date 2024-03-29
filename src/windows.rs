0f93183cae89423c6f5405ee416e50332f8560ac
sleefd <sleefd@gmail.com>
2024-03-29 21:12:29 +0800

    fix: fix file select on windows

--- src/windows.rs
+++ src/windows.rs
@@ -1,10 +1,12 @@
 use std::process::Command;
 
 pub fn open_folder_and_select_items(paths: &[&str]) -> bool {
-    let mut cmd = Command::new("explorer");
-    cmd.arg("/select,");
-    for path in paths {
-        cmd.arg(path);
-    }
-    cmd.output().unwrap().status
+    let mut cmd = Command::new("explorer.exe");
+    let arg = format!("/select,{}", paths.join(","));
+    cmd.arg(arg);
+    let result = cmd
+        .output()
+        .expect("Failed to run explorer.exe command");
+
+    result.status.success()
 }
