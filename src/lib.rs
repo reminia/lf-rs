9194d2c2dd2605ad7329ebc6f955e4417342b165
sleefd <sleefd@gmail.com>
2024-03-30 09:35:53 +0800

    feat: add unix module to support unix like os

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,50 @@ use windows as os;
 #[cfg(target_os = "macos")]
 use macos as os;
 
+#[cfg(any(
+target_os = "linux",
+target_os = "freebsd",
+target_os = "dragonfly",
+target_os = "netbsd",
+target_os = "openbsd",
+target_os = "illumos",
+target_os = "solaris"
+))]
+use unix as os;
+
 pub fn open(paths: &[&str]) -> bool {
     os::open_folder_and_select_items(paths)
 }
+
+#[cfg(not(any(
+target_os = "linux",
+target_os = "freebsd",
+target_os = "dragonfly",
+target_os = "netbsd",
+target_os = "openbsd",
+target_os = "illumos",
+target_os = "solaris",
+target_os = "ios",
+target_os = "macos",
+target_os = "windows",
+target_os = "haiku",
+target_os = "redox"
+)))]
+compile_error!("open is not supported on this platform");
 
 #[cfg(target_os = "windows")]
 mod windows;
 
 #[cfg(target_os = "macos")]
 mod macos;
+
+#[cfg(any(
+target_os = "linux",
+target_os = "freebsd",
+target_os = "dragonfly",
+target_os = "netbsd",
+target_os = "openbsd",
+target_os = "illumos",
+target_os = "solaris"
+))]
+mod unix;
---
+++ src/unix.rs
@@ -0,0 +1,5 @@
+use std::process::Command;
+
+pub fn open_folder_and_select_items(paths: &[&str]) -> bool {
+    true
+}
