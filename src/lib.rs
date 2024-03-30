e87473a41e6b8dc142a35fb56b57a13ccd2b366b
sleefd <sleefd@gmail.com>
2024-03-30 12:35:55 +0800

    refactor: target one unix os for now
    also fmt

--- src/lib.rs
+++ src/lib.rs
@@ -4,20 +4,14 @@ use windows as os;
 #[cfg(target_os = "macos")]
 use macos as os;
 
-#[cfg(any(
-target_os = "linux",
-))]
+#[cfg(target_os = "linux")]
 use unix as os;
 
 pub fn open(paths: &[&str]) -> bool {
     os::open_folder_and_select_items(paths)
 }
 
-#[cfg(not(any(
-target_os = "linux",
-target_os = "macos",
-target_os = "windows",
-)))]
+#[cfg(not(any(target_os = "linux", target_os = "macos", target_os = "windows",)))]
 compile_error!("open is not supported on this platform");
 
 #[cfg(target_os = "windows")]
@@ -26,7 +20,5 @@ mod windows;
 #[cfg(target_os = "macos")]
 mod macos;
 
-#[cfg(any(
-target_os = "linux",
-))]
+#[cfg(target_os = "linux")]
 mod unix;
