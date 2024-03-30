361f091d2671f6070975ae9bfdd0cd5a3fbd844b
sleefd <sleefd@gmail.com>
2024-03-30 12:09:18 +0800

    feat: done unix support

--- src/unix.rs
+++ src/unix.rs
@@ -4,10 +4,16 @@ pub fn open_folder_and_select_items(paths: &[&str]) -> bool {
     if paths.len() > 1 {
         println!("unix supports to select only 1 file.")
     }
-    let cmd = Command::new("nautilus");
-    let arg = format!("-s {}", paths.join(","));
-    let result = cmd.arg(arg)
+    let mut cmd = Command::new("nautilus");
+    cmd.arg("-s").arg(paths[0]);
+    let result = cmd
         .output()
         .expect("Fail to execute nautilus");
+    // debug cmd
+    if !result.status.success() {
+        if let Some(stderr) = String::from_utf8(result.stderr).ok() {
+            println!("stderr: {}", stderr);
+        }
+    }
     result.status.success()
 }
